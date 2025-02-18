f4c2997dab692b183b1e5b38f3afa00a01ae537e
nilaycm <168765867+nilaycm@users.noreply.github.com>
2025-02-18 18:17:12 +0100

    Adding a new hero

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,46 @@
 import SwiftUI
+import SwiftData
+
+@Model class Hero: Identifiable, Hashable {
+    var id: String
+    var logo: String 
+    
+    init(id: String, logo: String) {
+        self.id = id
+        self.logo = logo
+    }
+}
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var heroes: [Hero]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            list
+                .toolbar {
+                    Button("+") {
+                        modelContext.insert(Hero(id: "new Hero", logo: "person.fill"))
+                    }
+                    .font(.largeTitle)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    var list: some View {
+        if heroes.isEmpty {
+            ContentUnavailableView("No heroes yet", systemImage: "person.fill.questionmark", description:  Text("Tap the + buttton to add your first hero"))
+        } else {
+            List(heroes) { hero in
+                HStack {
+                    Image(systemName: hero.logo)
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Spacer()
+                    Text(hero.id)
+                }
+            }
         }
     }
 }
--- MyApp.swift
+++ MyApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct MyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([Hero.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(sharedModelContainer)
         }
     }
 }
